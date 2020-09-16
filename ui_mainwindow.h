9e19ff40f407515da952875dc5546ee3537ccc99
Rhys Bartholomaeus <rhys.bartholomaeus@gmail.com>
2020-09-16 18:24:25 +1000

    Updated layout of program

--- ui_mainwindow.h
+++ ui_mainwindow.h
@@ -11,6 +11,7 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QGridLayout>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QListWidget>
 #include <QtWidgets/QMainWindow>
@@ -27,15 +28,15 @@ class Ui_MainWindow
 public:
     QWidget *centralwidget;
     QVBoxLayout *verticalLayout;
-    QWidget *tacDisplay;
-    QListWidget *overlayList;
+    QGridLayout *gridLayout;
     QLabel *overlayLabel;
-    QPushButton *clearAllDrawings;
+    QListWidget *overlayList;
+    QGridLayout *gridLayout_2;
     QPushButton *trackQueryInit;
-    QPushButton *spawnPlane;
-    QPushButton *pushButton_4;
-    QPushButton *pushButton_3;
+    QPushButton *clearAllDrawings;
     QPushButton *clearAllTracks;
+    QPushButton *spawnPlane;
+    QWidget *tacDisplay;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -44,68 +45,74 @@ public:
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
         MainWindow->setEnabled(true);
-        MainWindow->resize(1024, 768);
+        MainWindow->resize(1081, 652);
+        MainWindow->setMinimumSize(QSize(1081, 652));
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         centralwidget->setMinimumSize(QSize(800, 559));
         verticalLayout = new QVBoxLayout(centralwidget);
         verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
-        tacDisplay = new QWidget(centralwidget);
-        tacDisplay->setObjectName(QString::fromUtf8("tacDisplay"));
-        QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-        sizePolicy.setHorizontalStretch(0);
-        sizePolicy.setVerticalStretch(0);
-        sizePolicy.setHeightForWidth(tacDisplay->sizePolicy().hasHeightForWidth());
-        tacDisplay->setSizePolicy(sizePolicy);
-        tacDisplay->setMinimumSize(QSize(800, 600));
-
-        verticalLayout->addWidget(tacDisplay);
-
-        overlayList = new QListWidget(centralwidget);
-        overlayList->setObjectName(QString::fromUtf8("overlayList"));
-
-        verticalLayout->addWidget(overlayList);
-
+        gridLayout = new QGridLayout();
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
         overlayLabel = new QLabel(centralwidget);
         overlayLabel->setObjectName(QString::fromUtf8("overlayLabel"));
         overlayLabel->setLayoutDirection(Qt::LeftToRight);
 
-        verticalLayout->addWidget(overlayLabel);
+        gridLayout->addWidget(overlayLabel, 1, 1, 1, 1);
+
+        overlayList = new QListWidget(centralwidget);
+        overlayList->setObjectName(QString::fromUtf8("overlayList"));
+        QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Expanding);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(overlayList->sizePolicy().hasHeightForWidth());
+        overlayList->setSizePolicy(sizePolicy);
+
+        gridLayout->addWidget(overlayList, 2, 1, 1, 1);
+
+        gridLayout_2 = new QGridLayout();
+        gridLayout_2->setObjectName(QString::fromUtf8("gridLayout_2"));
+        trackQueryInit = new QPushButton(centralwidget);
+        trackQueryInit->setObjectName(QString::fromUtf8("trackQueryInit"));
+
+        gridLayout_2->addWidget(trackQueryInit, 2, 0, 1, 1);
 
         clearAllDrawings = new QPushButton(centralwidget);
         clearAllDrawings->setObjectName(QString::fromUtf8("clearAllDrawings"));
 
-        verticalLayout->addWidget(clearAllDrawings);
-
-        trackQueryInit = new QPushButton(centralwidget);
-        trackQueryInit->setObjectName(QString::fromUtf8("trackQueryInit"));
-
-        verticalLayout->addWidget(trackQueryInit);
-
-        spawnPlane = new QPushButton(centralwidget);
-        spawnPlane->setObjectName(QString::fromUtf8("spawnPlane"));
-
-        verticalLayout->addWidget(spawnPlane);
-
-        pushButton_4 = new QPushButton(centralwidget);
-        pushButton_4->setObjectName(QString::fromUtf8("pushButton_4"));
-
-        verticalLayout->addWidget(pushButton_4);
-
-        pushButton_3 = new QPushButton(centralwidget);
-        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
-
-        verticalLayout->addWidget(pushButton_3);
+        gridLayout_2->addWidget(clearAllDrawings, 2, 1, 1, 1);
 
         clearAllTracks = new QPushButton(centralwidget);
         clearAllTracks->setObjectName(QString::fromUtf8("clearAllTracks"));
 
-        verticalLayout->addWidget(clearAllTracks);
+        gridLayout_2->addWidget(clearAllTracks, 0, 0, 1, 1);
+
+        spawnPlane = new QPushButton(centralwidget);
+        spawnPlane->setObjectName(QString::fromUtf8("spawnPlane"));
+
+        gridLayout_2->addWidget(spawnPlane, 0, 1, 1, 1);
+
+
+        gridLayout->addLayout(gridLayout_2, 0, 1, 1, 1);
+
+        tacDisplay = new QWidget(centralwidget);
+        tacDisplay->setObjectName(QString::fromUtf8("tacDisplay"));
+        QSizePolicy sizePolicy1(QSizePolicy::Expanding, QSizePolicy::Expanding);
+        sizePolicy1.setHorizontalStretch(1);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(tacDisplay->sizePolicy().hasHeightForWidth());
+        tacDisplay->setSizePolicy(sizePolicy1);
+        tacDisplay->setMinimumSize(QSize(800, 600));
+
+        gridLayout->addWidget(tacDisplay, 0, 2, 3, 1);
+
+
+        verticalLayout->addLayout(gridLayout);
 
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 1024, 21));
+        menubar->setGeometry(QRect(0, 0, 1081, 21));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -120,12 +127,10 @@ public:
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
         overlayLabel->setText(QApplication::translate("MainWindow", "Overlays:", nullptr));
-        clearAllDrawings->setText(QApplication::translate("MainWindow", "Clear All Drawings", nullptr));
         trackQueryInit->setText(QApplication::translate("MainWindow", "Query Tracks", nullptr));
-        spawnPlane->setText(QApplication::translate("MainWindow", "Spawn Plane", nullptr));
-        pushButton_4->setText(QApplication::translate("MainWindow", "PushButton", nullptr));
-        pushButton_3->setText(QApplication::translate("MainWindow", "PushButton", nullptr));
+        clearAllDrawings->setText(QApplication::translate("MainWindow", "Clear All Drawings", nullptr));
         clearAllTracks->setText(QApplication::translate("MainWindow", "Clear All Tracks", nullptr));
+        spawnPlane->setText(QApplication::translate("MainWindow", "Spawn Plane", nullptr));
     } // retranslateUi
 
 };
